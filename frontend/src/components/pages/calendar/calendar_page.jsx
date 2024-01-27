20adf79e67c9623e0dc4f1ea6148b92b72246903
tenor-ow <76924426+tenor-ow@users.noreply.github.com>
2024-01-27 15:51:22 -0500

    home and calendar works

---
+++ frontend/src/components/pages/calendar/calendar_page.jsx
@@ -0,0 +1,12 @@
+import Calendar from "./calendar_component.jsx";
+function Calendar_page(){
+    return (
+        <>
+            <Calendar />
+        </>
+
+    );
+
+
+}
+export default Calendar_page
