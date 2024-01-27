6ddf169dd9cd482c3bcaa398ef445e3768cdafaf
gavindg <52291693+gavindg@users.noreply.github.com>
2024-01-27 12:28:37 -0800

    Refactoring

--- backend/scraper/dining_scraper.py
+++ backend/scraper/dining_scraper.py
@@ -7,7 +7,12 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+
+# constants
 BASE_URL = 'https://uci.campusdish.com/LocationsAndMenus/'
+MAX_TIMEOUT = 10
+POLL_FREQ = 0.2
+COOKIES_SLEEP = 5
 
 
 def scrape_dining_data(eatery_id):
@@ -17,36 +22,49 @@ def scrape_dining_data(eatery_id):
     browser.get(BASE_URL + eatery_id)
 
     close_popups(browser)
-    scrape_current_source(browser)
+    # scrape_current_source(browser)
+    switch_page(to=None, browser=browser)
 
 
 # switch page to the next day...
-def switch_contexts(browser, to):
-    pass
+def switch_page(to, browser):
+    # first find the "change" button. we might need to scroll down to it.
+    errors = [NoSuchElementException]
+    change = (WebDriverWait(browser, timeout=MAX_TIMEOUT, poll_frequency=POLL_FREQ, ignored_exceptions=errors)
+              .until(EC.presence_of_element_located((By.CLASS_NAME, "DateMealFilterButton"))))
+    print(type(change))
+    click_elem(change, browser)
 
 
-# this function does some inital setup once it hits the first page, like clearing
+# clicks an element. more reliable than element.click()
+def click_elem(elem, browser):
+    browser.execute_script("arguments[0].click();", elem)
+
+
+# this function does some initial setup once it hits the first page, like clearing
 # the cookies preferences and the popup that opens.
 # this should only needs to be done once per session.
 def close_popups(browser):
     errors = [NoSuchElementException]
     # close out of the popup
-    (WebDriverWait(browser, timeout=10, poll_frequency=0.2, ignored_exceptions=errors)
+    (WebDriverWait(browser, timeout=MAX_TIMEOUT, poll_frequency=POLL_FREQ, ignored_exceptions=errors)
      .until(EC.presence_of_element_located((By.CLASS_NAME, "sc-ikkxIA"))).click())  # can only search by 1 classname @ a time.. good to know
     # close out of the cookies popup
-    (WebDriverWait(browser, timeout=10, poll_frequency=1, ignored_exceptions=errors)
+    (WebDriverWait(browser, timeout=MAX_TIMEOUT, poll_frequency=POLL_FREQ, ignored_exceptions=errors)
      .until(EC.presence_of_element_located((By.CLASS_NAME, "banner-close-button"))).click())
+
+    time.sleep(COOKIES_SLEEP) # big sleep here because there's some sort of JS reload after cookies menu is closed...
 
 
 def scrape_current_source(browser):
     source = browser.page_source
     soup = BeautifulSoup(source, "html.parser")
 
-    parentcategories = soup.find_all("div", {"class": "sc-dkmUuB jOJEgu MenuParentCategory"})
-    if parentcategories is None:
+    parent_categories = soup.find_all("div", {"class": "sc-dkmUuB jOJEgu MenuParentCategory"})
+    if parent_categories is None:
         print("no parents LOL")
 
-    for parent in parentcategories:
+    for parent in parent_categories:
         name = parent.find("h2", {"class": "sc-bDumWk gBTLmS CategoryName"})
         print(name.text.strip())
         contents = parent.find_all("h3", {"class": "sc-fXSgeo htxwrt HeaderItemName"})
@@ -55,9 +73,9 @@ def scrape_current_source(browser):
         print("\n")
 
 
+# for testing purposes only
 def main():
     scrape_dining_data("BrandyWine")
-    pass  # for testing purposes only
 
 
 if __name__ == "__main__":
