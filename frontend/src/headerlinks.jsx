b7feb00b5e787aa4f0efa06a4ec9ec11f591a9ac
tenor-ow <76924426+tenor-ow@users.noreply.github.com>
2024-01-27 02:57:56 -0500

    made a headers document, idk what to do now

---
+++ frontend/src/headerlinks.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+/* TODO: Figure out how to make multiple pages
+    this is just gonna be a viewable thing
+
+
+*/
+
+function Header(){
+    return(
+        <>
+            <p> gi</p>
+        </>
+    );
+
+}
+export default Header
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Calender from './App.jsx'
+import Header from './headerlinks.jsx'
 
-
+/* Header doesnt make sense */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <Calender />
   </React.StrictMode>,
 )
