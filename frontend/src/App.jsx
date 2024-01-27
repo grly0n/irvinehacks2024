18aa640a0cefb579a86971edc63f40c0ee3cf4a6
tenor-ow <76924426+tenor-ow@users.noreply.github.com>
2024-01-27 02:42:30 -0500

    framework of the website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
 
+import "./App.css"
+
+/* pipe in the data here*/
+const data=["meatloaf","taco", "breadstick", "meatloaf","meatloaf","meatloaf","meatloaf"]
+
 
 function Calender() {
   const date= new Date();
@@ -6,20 +11,37 @@ function Calender() {
   const current_day=date.getDate();
   const current_year=date.getFullYear()
 
-  
+  let mon= data[0]
+  let tues = data[1]
+  let wed = data[2]
+  let thurs = data[3]
+  let fri = data[4]
+  let sat = data[5]
+  let sun = data[6]
   return (
     <>
       
-      <table className='Calends'>
+      <table  className='Calend'>
         <tr>
-          <th>Monday</th>
-          <th>Tuesday</th>
-          <th>Wednesday</th>
-          <th>Thursday</th>
-          <th>Friday</th>
-          <th>Saturday</th>
-          <th>Sunday</th>
+          <th className='days'>Monday</th>
+          <th className='days'>Tuesday</th>
+          <th className='days'>Wednesday</th>
+          <th className='days'>Thursday</th>
+          <th className='days'>Friday</th>
+          <th className='days'>Saturday</th>
+          <th className='days'>Sunday</th>
 
+
+        </tr>
+        
+        <tr>
+          <th className='food'>{mon} </th>
+          <th className='food'>{tues} </th>
+          <th className='food'>{wed} </th>
+          <th className='food'>{thurs} </th>
+          <th className='food'>{fri} </th>
+          <th className='food'>{sat} </th>
+          <th className='food'>{sun} </th>
 
         </tr>
 
