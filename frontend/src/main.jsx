dbb27eeae214ce95802944087a9a0ac926243c2b
tenor-ow <76924426+tenor-ow@users.noreply.github.com>
2024-01-27 01:32:28 -0500

    changed the directory of everything

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Calender from './App.jsx'
 
-/*
-This code renders our project so it can be viewed in a browser. 
-*/
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Calender />
+  </React.StrictMode>,
+)
