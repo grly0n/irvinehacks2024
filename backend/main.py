6105422d48ca7f8420bc5e51df18d9f3033f74b7
George Lyon <grlyon@uci.edu>
2024-01-27 14:28:14 -0800

    Implemented dictionary over SQL

--- backend/main.py
+++ backend/main.py
@@ -5,71 +5,79 @@ This one file is all you need to start off with your FastAPI server!
 """
 
 from typing_extensions import Annotated
-import time
+from pydantic import BaseModel
+import datetime
+
 
 import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import session
-from sql_app import crud, models, schemas
-from sql_app.database import SessionLocal, engine
+from fastapi.responses import HTMLResponse
+
 
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
 
-# Database dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+class Meal(BaseModel):
+    id: int
+    location: str
+    time: str
+    type: str
+
+
+meals_db = dict()
+
+
+def ensure_no_duplicate(id: int, db: dict[int, Meal]) -> bool:
+    for entry in db:
+        if entry == id: return False
+    return True
+
+
+@app.get("/meals", tags=["meals"])
+async def get_meals():
+    return {"data": meals_db}
+
+
+@app.post("/meals", tags=["meals"])
+async def add_meal(meal: Meal):
+    if not ensure_no_duplicate(meal.id, meals_db):
+        return {"data": "Meal not added, duplicate ID found"}
+    meals_db[meal.id] = meal
+    return {"data": "Meal added"}
+
+
+@app.put("/meals", tags=["meals"])
+async def update_meals(id: int, newMeal: Meal):
+    for meal in meals_db:
+        if meal == id:
+            meals_db[id] = newMeal
+            return {"data": f"Meal with id {id} updated"}
+    return {"data": f"Meal with id {id} not found"}
+
+
+@app.delete("/meals", tags=["meals"])
+async def remove_meal(id: int):
+    for meal in meals_db:
+        if meal == id:
+            meals_db.pop(id)
+            return {"data": f"Meal with id {id} removed"}
+    return {"data": f"Meal with id {id} not found"}
 
 
 @app.get("/")
 async def main():
-    return {"Hello": "world!"}
-
+    content = """
+    <body>
+        <h1>Welcome to Dining Hall Menu 2!</h1>
+        <p>This is a placeholder for the home screen which will go here soon</p>
+    </body>
+"""
+    return HTMLResponse(content)
 
 @app.get("/home")
 def home():
     return {"message": "This is the home page"}
-
-
-@app.post("/locations/", response_model=schemas.Location)
-def create_location(location: schemas.LocationCreate, db: session = Depends(get_db)):
-    db_location = crud.get_location(db, location.id)
-    if db_location:
-        raise HTTPException(status_code=400, detail="Location already registered")
-    return crud.create_location(db=db, location=location)
-
-
-@app.get("/locations/", response_model=list[schemas.Location])
-def read_locations(skip: int = 0, limit: int = 100, db: session = Depends(get_db)):
-    locations = crud.get_locations(db, skip=skip, limit=limit)
-    return locations
-
-
-@app.get("/locations/{location_id}", response_model=schemas.Location)
-def read_location(location_id: int, db: session = Depends(get_db)):
-    db_location = crud.get_location(db, location_id=location_id)
-    if db_location is None:
-        raise HTTPException(status_code=404, detail="Location not found")
-    return db_location
-
-
-@app.post("/locations/{location_id}/meals/", response_model=schemas.Meal)
-def create_meal_for_location(
-    location_id: int, meal: schemas.MealCreate, db: session = Depends(get_db)
-):
-    return crud.create_meal(db, meal=meal, location_id=location_id)
-
-
-@app.get("/meals/", response_model=list[schemas.Meal])
-def read_meals(skip: int = 0, limit: int = 100, db: session = Depends(get_db)):
-    meals = crud.get_meals(db, skip=skip, limit=limit)
-    return meals
 
 
 if __name__ == "__main__":
