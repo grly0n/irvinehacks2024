09ce6ea7d53607bada3ee19f19d0bfe411d850ff
George Lyon <grlyon@uci.edu>
2024-01-27 22:36:00 -0800

    Preparation for backend implementation

--- backend/api_functions.py
+++ backend/api_functions.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, File
+from fastapi import FastAPI, File
 from fastapi.responses import HTMLResponse
 import database.database as db
 
@@ -8,7 +8,8 @@ from typing import Annotated
 All backend-to-frontend function calls are kept here
 """
 app = FastAPI()
-db.meals_db = dict()
+brandy_meals_db = dict()
+anteat_meals_db = dict()
 
 # Placeholder main page
 @app.get("/")
@@ -16,9 +17,9 @@ async def main():
     content = """
     <body>
         <h1>Welcome to BrandyEatery!</h1>
-        <p>Please upload database file</p>
-        <form action="/load-from-database/" enctype="multipart/form-data" method="post">
-        <input name="file" type = "file">
+        <p>Please upload database files</p>
+        <form action="/files/" enctype="multipart/form-data" method="post">
+        <input name="files" type = "file" multiple>
         <input type="submit">
         </form>
     </body>
@@ -28,10 +29,12 @@ async def main():
 # Returns all meals logged in the database
 @app.get("/meals", tags=["meals"])
 async def get_meals():
-    return {"data": db.meals_db}
+    return {"anteatery_db" : anteat_meals_db, "brandywine_db" : brandy_meals_db}
 
-# Load database from a text file
-@app.post("/load-from-database/", tags=["database"])
-async def load_from_database(file: Annotated[bytes, File()]):
-    db.meals_db = db.read_from_file(file)
-    return {"message" : "Database loaded", "database" : db.meals_db}
+
+# Load databases from a text file
+@app.post("/files/")
+async def load_from_database(files: Annotated[list[bytes], File(description="Multiple files as bytes")]):
+    brandy_meals_db = db.read_from_file(files[1])
+    anteat_meals_db = db.read_from_file(files[0])
+    return {"anteatery_db" : anteat_meals_db, "brandywine_db" : brandy_meals_db}
--- backend/database/database.py
+++ backend/database/database.py
@@ -1,7 +1,9 @@
 import json
+import scraper
 
-meals_db = dict() # Structure: meals_db[days_ahead][location][meal][station][food]
+# Structure of database: db[(month, day)][meal][station][food]
 
 # Returns a database from the loaded pseudo-JSON (txt) file
-def read_from_file(file : bytes) -> dict[(int, int), dict[str, dict[str, dict[str, list[str]]]]]:
+def read_from_file(file : bytes) -> dict[(int, int), dict[str, dict[str, list[str]]]]:
     return json.dumps(file.decode())
+
